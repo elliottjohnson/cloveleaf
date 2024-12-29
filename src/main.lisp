900cb0f9fb16104a20939e9bea04fcaab5e3ae1e
Elliott Johnson <elliott@elliottjohnson.net>
2024-12-29 00:36:14 -0800

    Adding the basics of SMuFL glyphs.

--- src/main.lisp
+++ src/main.lisp
@@ -5,6 +5,10 @@
 (defvar *smufl-specification-version* "1.40"
   "A version string to identify the SMuFL specification version this library is compatible with.")
 
+;;;
+;;; Font clases
+;;;
+
 (defclass font ()
   ((name :accessor font-name
 	 :documentation "A display name string for this font."
@@ -55,3 +59,59 @@
 	     :initarg :metadata
 	     :type pathname))
   (:documentation "A class for all SMuFL fonts that need an external metadata file to provide information."))
+
+
+;;;
+;;; Glyph classes
+;;;
+
+(defclass glyph ()
+  ((name :accessor glyph-name
+	 :documentation "A namestring representing the glyph name."
+	 :initarg :name
+	 :type string)
+   (code-point :accessor glyph-code-point
+	       :documentation "A string representing the unicode codepoint."
+	       :initarg :code-point
+	       :type string)
+   (code :accessor glyph-code
+	 :documentation "A code is a number used to represent this glyph."
+	 :initarg :code
+	 :type number)
+   (character :accessor glyph-character
+	      :documentation "A unicode character to represent this glyph."
+	      :initarg :character
+	      :type character)
+   (description :accessor glyph-description
+		:documentation "A string descriptions of the character."
+		:initarg :description
+		:type string)
+   
+   (alternates :accessor glyph-altenates
+	       :documentation "A list of alternates for this character."
+	       :initarg :alternates
+	       :initform ()
+	       :type list)
+   (alternate-for :accessor glyph-alternate-for
+		  :documentation "This glyph is an alternate for other glyphs."
+		  :initarg :alternate-for
+		  :initform ()
+		  :type list)
+   (advanced-width :accessor glyph-advanced-width
+		   :documentation "Defines a glyph width from the origin of the glyph in the next glyph."
+		   :initarg :advanced-with
+		   :type number)
+   (anchors :accessor glyph-anchors
+	    :documentation "A list of up to 22 attributes wand values forthe given glyph."
+	    :initarg :glyph-anchors
+	    :type list)
+   (bounding-boxes :accessor glyph-bounding-box
+		   :documentation "Defines the bounding box of the glyph or smallest rectaingle test encloses very part of the path."
+		   :initarg :bounding-boxes))
+    (:documentation "A class to represent the SMuFL spec's defined glyphs."))
+
+(defclass ligature-glyph (glyph)
+  ((component-glyphs :accessor glyph-ligature-glyph-componts-flyths
+		     :documentation "A list of glyphs that make up a ligature for this graph."
+		     :initarg :ligature))
+  (:documentation "A class for all ligature classes"))
