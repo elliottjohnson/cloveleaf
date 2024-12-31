d409826c5877b91d9aea912ba38316aae557ed57
Elliott Johnson <elliott@elliottjohnson.net>
2024-12-30 21:28:55 -0800

    Adding some variables and parameters to define the SMuFL libraries metadata files.

--- build/generate.lisp
+++ build/generate.lisp
@@ -2,3 +2,16 @@
   (:use #:cl #:com.inuoe.jzon))
 (in-package #:cloveleaf/smufl)
 
+(defvar *smufl-metadata-directory-pathname*
+  nil
+  "A default pathname to use for the SMuFL metadata.")
+
+(defparameter +smufl-metadata-glyphnames-filename+
+  "glyphnames.json"
+  "The filename of the SMuFL glyphnames.json file to read in.")
+(defparameter +smufl-metadata-classes-filename+
+  "classes.json"
+  "The filename of the SMuFL classes.json file to read in.")
+(defparameter +smufl-metadata-classes-ranges.json+
+  "ranges.json"
+  "The filename of the SMuFL ranges.json file to read in.")
