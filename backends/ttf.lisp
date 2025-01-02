bed179aad90461611815e10a57f8b45c156b3580
Elliott Johnson <elliott@elliottjohnson.net>
2025-01-02 00:09:30 -0800

    Roughing in the TTF and font loading code.  Unfinished, but commiting to capture progress.

--- backends/ttf.lisp
+++ backends/ttf.lisp
@@ -1,4 +1,18 @@
 (uiop:define-package cloveleaf/ttf
-  (:use #:cl #:zpb-ttf))
+  (:use #:cl #:cloveleaf #:zpb-ttf)
+  (:export #:ttf-font))
 (in-package :cloveleaf/ttf)
 
+(defclass ttf-font (metadata-font)
+  ()
+  (:documentation "A class for all SMuFL fonts using True Type Font formats."))
+
+(defmethod load-glyphs ((font ttf-font) &key data &allow-other-keys)
+  (declare (ignore data))
+  (with-font-loader (font-loader (font-pathname font))
+    (loop for i from 0 to (1- (glyph-count font-loader))
+	  with ttf-glyph = (index-glyph i font-loader)
+	  do (destructuring-bind (sym &rest glyphdata)
+		 glyph
+	       (let ((codepoint (cdr (assoc :codepoint glyphdata))))
+		 )))))
--- src/font.lisp
+++ src/font.lisp
@@ -22,6 +22,11 @@
 	       :initarg :glyph-hash
 	       :type hashtable
 	       :initform (make-hash-table :test #'eq))
+   (name-hash :accessor font-name-hash
+	      :documentation "A hashtable that holds all of the associated font's glyphs hashed by name."
+	      :initarg :name-hash
+	      :type hashtable
+	      :initform (make-hash-table :test #'equal))
    (engraving-defaults :accessor font-engraving-defaults
 		       :documentation "The engraving defaults for this font."
 		       :initarg :engraving-defaults)
@@ -54,3 +59,96 @@
 	     :initarg :metadata
 	     :type pathname))
   (:documentation "A class for all SMuFL fonts that need an external metadata file to provide information."))
+
+(defgeneric clear-font (font &key total)
+  (:method ((font font) &key total)
+    (flet ((font-slot-makunbound (s)
+	     (slot-makunbound font s)))
+      (loop for slot in '(name
+			  version
+			  glyph-hash
+			  engraving-defaults
+			  design-size
+			  size-range
+			  classes
+			  ranges
+			  sets)
+	    do (font-slot-makunbound slot))
+      (when total (font-slot-makunbound 'pathname))))
+  (:method :around ((font metadata-font) &key total)
+    (when total (slot-makunbound font 'metadata))
+    (call-next-method))
+  (:documentation
+   "Clears out all data that is parsed in to create the font definition.
+If TOTAL is true, then the entire font-object is cleared, including
+slots, which maybe required to locate the font definition."))
+
+(defgeneric load-font (font &key clear)
+  (:method :before ((font font) &key clear)
+    (when clear (clear-font font)))
+  (:method ((font metadata-font) &key clear)
+    (declare (ignore clear))
+    (assert (pathnamep (font-metadata font)))
+    (let ((metadata (parse (font-metadata font))))
+      (parse-font font :data metadata)
+      (load-glyphs font metadata)
+      (assign-alternate-glyphs font metadata)
+      (assign-ligature-glyphs font metadata)
+      (assign-optional-glyphs font metadata)
+      (assign-glyph-sets font metadata)
+      (set-glyph-advanced-widths font metadata)
+      (set-glyph-anchors font metadata)
+      (set-alternates font metadata)
+      (set-bounding-boxes font metadata)
+      (associate-classes font metadata)
+      (associate-ranges font metadata)))
+  (:documentation
+   "A generic function for loading fonts and the various tasks associated."))  
+
+(defgeneric parse-font (font &key data &allow-other-keys)
+  (:method ((font metadata-font) &key data &allow-other-keys)
+    (assert (hash-table-p data))
+    (flet ((datahash (key) (gethash key data)))
+      (with-accessors ((name font-name)
+		     (version font-version)
+		     (engraving-defaults font-engraving-defaults)
+		     (design-size font-design-size)
+		     (size-range font-size-range))
+	  font 
+	(setf name (datahash "fontName")
+	      version (datahash "fontVersion")
+	      engraving-defaults (datahash "engravingDefaults")
+	      design-size (datahash "designSize")
+	      size-range (datahash "sizeRange"))))
+    ;; Todo, there can be optional key/value pairs here... capture these somehow?
+    font)
+  (:documentation
+   "Populates data about the font.  DATA can be provided if the font itself does
+not contain the information.  Other keys are allowed for future functionality.
+Aside from setting slots in the FONT object, the FONT is returned as a value."))
+
+(defgeneric load-glyphs (font &key data &allow-other-keys)
+  (:documentation
+   "A backend dependent method for loading glyphs from a font file."))
+
+(defgeneric add-font-glyph (font glyph &optional error-p)
+  (:method ((font font) (glyph glyph) &optional (error-p t))
+    (with-accessors ((glyph-hash font-glyph-hash)
+		     (name-hash font-name-hash))
+	font
+      (assert (hash-table-p glyph-hash))
+      (assert (hash-table-p name-hash))
+      (with-accessors ((char glyph-character)
+		       (name glyph-name))
+	  glyph
+	(assert (characterp char))
+	(assert (stringp name))
+	(when (and error-p
+		   (or (gethash char glyph-hash)
+		       (gethash name name-hash)))
+	  (error "Glyph: ~A already is hashed for font: ~A"
+		 name
+		 (font-name font)))
+	(setf (gethash char glyph-hash) glyph
+	      (gethash name name-hash) glyph))))
+  (:documentation "A method of adding a glyph to a font."))
